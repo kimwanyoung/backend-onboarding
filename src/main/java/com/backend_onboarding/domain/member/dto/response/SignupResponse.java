b80a15cf0c5aa28c9bf45d72baca2d3c723fa54c
kimwanyoung <dhks2869@gmail.com>
2025-01-19 14:56:58 +0900

    feat : 회원가입 기능 구현 - SignupResponse 클래스 of 메서드 추가 - MemberService signup 메서드 추가

--- src/main/java/com/backend_onboarding/domain/member/dto/response/SignupResponse.java
+++ src/main/java/com/backend_onboarding/domain/member/dto/response/SignupResponse.java
@@ -2,6 +2,7 @@ package com.backend_onboarding.domain.member.dto.response;
 
 import java.util.List;
 
+import com.backend_onboarding.domain.member.entity.Member;
 import com.backend_onboarding.domain.member.entity.MemberRole;
 
 import lombok.Builder;
@@ -12,6 +13,17 @@ public class SignupResponse {
 	private String username;
 	private String nickname;
 	private List<RoleResponse> authorities;
+
+	public static SignupResponse of(Member member) {
+		List<RoleResponse> authorities = List.of(RoleResponse.builder()
+			.authorityName(MemberRole.ROLE_USER)
+			.build());
+		return SignupResponse.builder()
+			.username(member.getUsername())
+			.nickname(member.getNickname())
+			.authorities(authorities)
+			.build();
+	}
 
 	@Data
 	public static class RoleResponse {
