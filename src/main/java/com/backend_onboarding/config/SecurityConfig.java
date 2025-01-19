0965ed441839a89ccaef0063bd9a92206d32697a
kimwanyoung <dhks2869@gmail.com>
2025-01-19 15:31:44 +0900

    feat : SecurityConfig에 LoginAuthenticationFilter 추가

--- src/main/java/com/backend_onboarding/config/SecurityConfig.java
+++ src/main/java/com/backend_onboarding/config/SecurityConfig.java
@@ -2,16 +2,26 @@ package com.backend_onboarding.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.backend_onboarding.security.filter.LoginAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+	private final AuthenticationConfiguration authenticationConfiguration;
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -21,10 +31,17 @@ public class SecurityConfig {
 			.authorizeHttpRequests((auth) -> auth
 				.requestMatchers("/sign", "/signup").permitAll() // 회원가입, 로그인 경로 인증 제외
 				.anyRequest().authenticated())
+			.addFilterAt(new LoginAuthenticationFilter(authenticationManager(authenticationConfiguration)),
+				UsernamePasswordAuthenticationFilter.class)
 			.sessionManagement((session) -> session
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // jwt 사용을 위해 세션 stateless 상태 설정
 
 		return http.build();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+		return configuration.getAuthenticationManager();
 	}
 
 	@Bean
